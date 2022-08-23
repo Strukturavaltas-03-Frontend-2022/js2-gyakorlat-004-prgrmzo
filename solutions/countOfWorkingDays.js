1ca98f21cdccaa3d2ffef4df551096b87e703e19
Zo <pahyzoard@outlook.hu>
2022-08-23 16:45:18 +0200

    workingdays done.

--- solutions/countOfWorkingDays.js
+++ solutions/countOfWorkingDays.js
@@ -1,1 +1,31 @@
+// 2. Írj egy függvényt `countOfWorkingDays` néven, ami két paramétert kap.
+//  Mind a két paraméter egy-egy dátum. Az első a  korábbi időpont, a második
+//   a későbbi időpont.  A visszatérési értéke a két dátum közötti munkanapok
+//   (hétfő-péntek) száma! A különböző ünnepekkel nem kell számolnod! Ha az első
+//    dátum korábbi, mint a második akkor dobj egy hibát az alábbi üzenettel:
+//    `The first parameter is earlier date, than second!`!
+// Hibát dobni a következő módon tudsz:
+
+// ```javascript
+//   throw new Error(message)
+// ```
+// A aktuális napot is bele kell számolni a munkanapokba, ha az munkanap!
+// Sehol ne használj kerekítést!
+
+const countOfWorkingDays = (
+  earlierDate = new Date(),
+  laterDate = new Date()
+) => {
+  if (earlierDate > laterDate) {
+    throw new Error(`The first parameter is earlier date, than second!`);
+  }
+  let workDays = 0;
+  laterDate.getDay() == 0 || laterDate.getDay() == 6 ? {} : (workDays = 1);
+  while (laterDate > earlierDate) {
+    earlierDate.getDay() == 0 || earlierDate.getDay() == 6 ? {} : workDays++;
+    earlierDate.setDate(earlierDate.getDate() + 1);
+  }
+  return workDays;
+};
+
 export default countOfWorkingDays;
--- solutions/getSecondsOfThisYearUntilNow.js
+++ solutions/getSecondsOfThisYearUntilNow.js
@@ -1,3 +1,13 @@
+// 1. Írj egy függvényt `getSecondsOfThisYearUntilNow` néven, aminek a visszatérési értéke az aktuális év 01.01. óta
+// eltelt másodperceinek a száma!
+//    Csak a Date konstruktor függvényt (akár példányosítva, akár anélkül), a now() és a getFullYear()
+//    metódusokat használd!
+
+const getSecondsOfThisYearUntilNow = () => {
+  const now = new Date();
+  const secondsUntilNow = Date.now() - new Date(now.getFullYear());
+
+  return secondsUntilNow / 1000;
+};
 
 export default getSecondsOfThisYearUntilNow;
-
